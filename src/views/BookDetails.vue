8fa3faa08f4167c6c66b016aa017b929c747a123
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-04-01 09:24:59 +0200

    prva stranica

---
+++ src/views/BookDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bookdetails">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  name: "BookDetails",
+  props: ["name"],
+
+  data: function () {
+    return {
+    }
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+    <li v-for="item in books" v-bind:key="item.url">
+      Ime: <router-link :to=" '/bookDetails/' + item.name"> {{ item.name }}</router-link> <br>
+      Autor: {{item.authors}} <br>
+      Izdano: {{item.released}} <br> <br>
+    </li>
+  </ul>
+    {{books}}
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView',
+  data: function () {
+    return {
+      books: [],
+    };
+  },
+  async mounted() {
+    let rezultat = await fetch(
+      'http://ntankovic.unipu.hr:8000/books.json'
+    );
+    let podaci = await rezultat.json();
+
+    this.books = podaci;
+  },
 }
 </script>
